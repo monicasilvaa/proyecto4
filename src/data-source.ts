662ee3e4ce4d5d5f3f60d3327d7c3713e5e76bae
monicasilvaa <monicasilva2307@gmail.com>
2024-01-03 20:05:16 +0100

    connection to the database: app.ts, server.ts, data-source.ts and routes

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3307,
+    username: "root",
+    password: "1234",
+    database: "estudioTatuajes",
+    entities: [],
+    migrations: [],
+    synchronize: false,
+    logging: false,
+ });
